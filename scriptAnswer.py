0b16214868846b91292937c72b0432292688667c
Alifo-Johnson <alifojohnson23@gmail.com>
2024-10-07 18:06:45 +0100

    Second commit to Praise

--- scriptAnswer.py
+++ scriptAnswer.py
@@ -1,16 +1,14 @@
-x = 5 + 4, y = 6 - 10, z = 7 * 4
+x = 5 + 4, 6 - 10, 7 * 4
 # I seperated the above different operations using comma
 
-print(x,y,z)
-
 # We have the floor operator (//)
- 4 // 2
+y = 4 // 2
 
 #The floor operations returns the Whole number afte a division exercise
 
 # We have the modulus operator
 
-7 % 3 
+z = 7 % 3 
 
 #The modulus operator returns the remainder of a divided operation
-
+print(x, y, z)
